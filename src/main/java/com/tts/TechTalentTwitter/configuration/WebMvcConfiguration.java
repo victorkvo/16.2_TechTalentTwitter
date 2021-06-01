114249d22eb8410bec0024bb70ae5591b7fcdad5
Victor Vo <79146455+victorkvo@users.noreply.github.com>
2021-06-01 14:26:51 -0500

    Initial commit

---
+++ src/main/java/com/tts/TechTalentTwitter/configuration/WebMvcConfiguration.java
@@ -0,0 +1,20 @@
+package com.tts.TechTalentTwitter.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        // below we're instantiating BycryptPasswordEncoder
+        // assigning it to a variable
+        // and we'll need to return it
+//        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+        // instead we can simply return the instantiation
+        return new BCryptPasswordEncoder();
+    }
+}
